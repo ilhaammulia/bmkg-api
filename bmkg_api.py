b5b6e0aa230a2e0b9395f4291047eaf285beab1c
Ilham Mulia <mail.ilhammulia@gmail.com>
2022-02-24 12:54:09 +0700

    Update bmkg_api.py

--- bmkg_api.py
+++ bmkg_api.py
@@ -17,7 +17,7 @@ class Api:
         """
         try:
             with requests.Session() as session:
-                with session.get(base_url + self.url[0], headers={'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1', 'Upgrade-Insecure-Requests': '1'}) as response:
+                with session.get(base_url + self.url[int(option)], headers={'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1', 'Upgrade-Insecure-Requests': '1'}) as response:
                     data = response.json()
                     return data
         except:
