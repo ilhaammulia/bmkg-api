5690a4fe411b596a6799fa405b6dc2fe02470f8f
Ilham Mulia <mail.ilhammulia@gmail.com>
2022-02-24 12:49:51 +0700

    Update bmkg_api.py

--- bmkg_api.py
+++ bmkg_api.py
@@ -2,8 +2,23 @@ import requests, json
 
 base_url = 'https://data.bmkg.go.id/DataMKG/TEWS/'
 
-class api:
-    def __init__(self) -> None:
-        pass
-    def autogempa(self):
-        pass
+class Api:
+    def __init__(self):
+        """
+        Initialize url for request
+        """
+        self.url = ['autogempa.json']
+
+    def request(self, option):
+        """
+        HTTP Request to BMKG API
+        Select:
+            0 = autogempa
+        """
+        try:
+            with requests.Session() as session:
+                with session.get(base_url + self.url[0], headers={'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1', 'Upgrade-Insecure-Requests': '1'}) as response:
+                    data = response.json()
+                    return data
+        except:
+            return None
